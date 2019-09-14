e2dc288cce0c53895b60a95f2a90a24c13975d08
Daniil <cossack1992@gmail.com>
2019-09-14 14:41:33 +0200

    MT: Added Solution structure

---
+++ Mentoring.OfficeBuilder.DAL/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mentoring.OfficeBuilder.DAL.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddOfficeDbDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services.AddDbContext<OfficeDbContext>(option => option.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
---
+++ Mentoring.OfficeBuilder.DAL/OfficeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mentoring.OfficeBuilder.DAL
+{
+    public class OfficeDbContext : DbContext
+    {
+        public OfficeDbContext(DbContextOptions<OfficeDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
